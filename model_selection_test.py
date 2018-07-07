7ba30a0546d3c628209e0983c3490d1cf8130ca6
tblume1992 <t-blume@hotmail.com>
2018-07-07 15:04:21 -0400

    Update model_selection_test.py

--- model_selection_test.py
+++ model_selection_test.py
@@ -8,7 +8,7 @@ Created on Thu Jul  5 15:08:39 2018
 import lr_simulation as simulate
 import RegressionModelSelection as RMS
 
-simulation = simulate.simulateData(n_features = 11, noise = 20, multicollinearity = 0, 
+simulation = simulate.simulateData(n_samples = 1000, n_features = 11, noise = 20, multicollinearity = 0, 
                                    mc_correlation = 0.9, n_informative = 5, random_state = None)
 
 Xhat, yhat,coefhat = simulation.make_Data()
